ddb8c3f67a3904ae46a5be63b7af9501c52cef0e
Alejandro Pezuk <alejandropezuk@gmail.com>
2022-11-24 16:38:06 -0300

    Puliendo el CRUD

--- src/main/java/edu/polo/qatar/entidades/Jugador.java
+++ src/main/java/edu/polo/qatar/entidades/Jugador.java
@@ -3,6 +3,7 @@ package edu.polo.qatar.entidades;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import java.util.*;
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -18,14 +19,19 @@ public class Jugador {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Campo obligatorio")
+    @Size(max = 250, message= "Nombre demasiado largo")
     private String nombre;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Temporal(TemporalType.DATE)
+    @NotNull(message = "Campo obligatorio")
+    @Past(message = "La fecha de nacimiento no puede ser futura")
     private Date fechaNacimiento;
 
     @ManyToOne
     @JsonBackReference
+    @NotNull(message = "Debe elegir un valor")
     private Seleccion seleccion;
 
     private String foto;
