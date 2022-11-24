72181c5f873a4a622c785eb500a97b7b11abbfb9
Alejandro Pezuk <alejandropezuk@gmail.com>
2022-11-24 16:37:09 -0300

    Puliendo el CRUD

--- src/main/java/edu/polo/qatar/controladores/JugadorControlador.java
+++ src/main/java/edu/polo/qatar/controladores/JugadorControlador.java
@@ -4,23 +4,27 @@ import edu.polo.qatar.entidades.*;
 import edu.polo.qatar.servicios.*;
 import java.io.*;
 import java.nio.file.Paths;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.*;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @RestController
 @RequestMapping("jugadores")
-public class JugadorControlador {
+public class JugadorControlador implements WebMvcConfigurer {
 
-    @Autowired
+	@Autowired
     JugadorServicio jugadorServicio;
 
     @Autowired
     SeleccionServicio seleccionServicio;
 
-    @GetMapping
+	@GetMapping
     private ModelAndView index()
     {
         ModelAndView maw = new ModelAndView();
@@ -31,7 +35,7 @@ public class JugadorControlador {
         return maw;
     }
 
-    @GetMapping("/{id}")
+	@GetMapping("/{id}")
     private ModelAndView one(@PathVariable("id") Long id)
     {
         ModelAndView maw = new ModelAndView();
@@ -42,8 +46,8 @@ public class JugadorControlador {
         return maw;
     }
 
-    @GetMapping("/crear")
-    public ModelAndView crear()
+	@GetMapping("/crear")
+	public ModelAndView crear(Jugador jugador)
     {
         ModelAndView maw = new ModelAndView();
         maw.setViewName("fragments/base");
@@ -51,50 +55,75 @@ public class JugadorControlador {
         maw.addObject("vista", "jugadores/crear");
         maw.addObject("selecciones", seleccionServicio.getAll());
         return maw;
-    }
+	}
 
-    @PostMapping
-    private RedirectView save(@RequestParam("archivo") MultipartFile archivo, Jugador jugador)
+	@PostMapping("/crear")
+	public ModelAndView guardar(@RequestParam("archivo") MultipartFile archivo, @Valid Jugador jugador, BindingResult br, RedirectAttributes ra)
     {
-        jugadorServicio.save(jugador);
+        if ( archivo.isEmpty() )
+			br.reject("archivo", "Por favor, cargue una imagen"); 
+
+		if ( br.hasErrors() ) {
+			return this.crear(jugador);
+		}
+
+		jugadorServicio.save(jugador);
 
         String tipo = archivo.getContentType();
         String extension = "." + tipo.substring(tipo.indexOf('/') + 1, tipo.length());
         String foto = jugador.getId() + extension;
         String path = Paths.get("src/main/resources/static/images/jugadores", foto).toAbsolutePath().toString();
+        ModelAndView maw = this.index();
 
         try {
             archivo.transferTo( new File(path) );
         } catch (Exception e) {
-            return new RedirectView("/jugadores");
+            maw.addObject("error", "No se pudo guardar la imagen");
+            return maw;
         }
 
         jugador.setFoto(foto);
         jugadorServicio.save(jugador);
-        return new RedirectView("/jugadores");
+        maw.addObject("exito", "Jugador guardado exitosamente");
+		return maw;
+	}
+
+	@GetMapping("/editar/{id}")
+    public ModelAndView editar(@PathVariable("id") Long id, Jugador jugador)
+    {
+        return this.editar(id, jugador, true);
     }
 
-    @GetMapping("/editar/{id}")
-    public ModelAndView editar(@PathVariable("id") Long id)
+    public ModelAndView editar(@PathVariable("id") Long id, Jugador jugador, boolean estaPersistido)
     {
         ModelAndView maw = new ModelAndView();
         maw.setViewName("fragments/base");
         maw.addObject("titulo", "Editar jugador");
         maw.addObject("vista", "jugadores/editar");
         maw.addObject("selecciones", seleccionServicio.getAll());
-        maw.addObject("jugador", jugadorServicio.getById(id));
+
+        if (estaPersistido)
+            maw.addObject("jugador", jugadorServicio.getById(id));
+        else
+            jugador.setFoto( jugadorServicio.getById(id).getFoto() );
+
         return maw;
     }
 
-    @PutMapping("/{id}")
-    private Object update(@PathVariable("id") Long id,
+    @PutMapping("/editar/{id}")
+    private ModelAndView update(@PathVariable("id") Long id,
     @RequestParam(value = "archivo", required = false) MultipartFile archivo,
-    @ModelAttribute Jugador jugador)
+    @Valid Jugador jugador, BindingResult br, RedirectAttributes ra)
     {
+        if ( br.hasErrors() ) {
+			return this.editar(id, jugador, false);
+		}
+
         Jugador registro = jugadorServicio.getById(id);
         registro.setNombre( jugador.getNombre() );
         registro.setFechaNacimiento( jugador.getFechaNacimiento() );
         registro.setSeleccion( jugador.getSeleccion() );
+        ModelAndView maw = this.index();
 
         if ( ! archivo.isEmpty() ) {
             String tipo = archivo.getContentType();
@@ -105,21 +134,25 @@ public class JugadorControlador {
             try {
                 archivo.transferTo( new File(path) );
             } catch (Exception e) {
-                return e.toString();
+                maw.addObject("error", "No se pudo guardar la imagen");
+                return maw;
             }
 
             registro.setFoto(foto);
         }
 
         jugadorServicio.save(registro);
-        return new RedirectView("/jugadores");
+        maw.addObject("exito", "Jugador editado exitosamente");
+		return maw;
     }
 
     @DeleteMapping("/{id}")
-    private RedirectView delete(@PathVariable("id") Long id)
+    private ModelAndView delete(@PathVariable("id") Long id)
     {
         jugadorServicio.delete(id);
-        return new RedirectView("/jugadores");
+        ModelAndView maw = this.index();
+        maw.addObject("exito", "Jugador borrado exitosamente");
+		return maw;
     }
     
 }
